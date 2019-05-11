787acd69ed60327de3284167f1a915c5dcf3f521
Rob Fitzpatrick <robert.fitzpatrick@thalesesecurity.com>
2019-05-11 11:50:45 +0100

    Broken 64

--- bs/pack.c
+++ bs/pack.c
@@ -1,30 +1,30 @@
 #include "pack.h"
 
-void pack(uint32_t *out, const uint8_t *in) {
+void pack(uint64_t *out, const uint8_t *in) {
 
-        memset(out, 0x00, 8 * sizeof(uint32_t));
+        memset(out, 0x00, 8 * sizeof(uint64_t));
 
 	for(uint8_t i = 0; i < 8; i++) {
-	    for(uint8_t j = 0; j < 32; j++) {
+	    for(uint8_t j = 0; j < 64; j++) {
 		if((i - j) < 0) {
-			out[i] |= ((in[j] & (0x01 << i)) << (j - i));
+			out[i] |= (uint64_t)(((in[j] & (0x0000000000000001 << i)) << (j - i)));
 		} else {
-		        out[i] |= ((in[j] & (0x01 << i)) >> (i - j));
+		        out[i] |= (uint64_t)(((in[j] & (0x0000000000000001 << i)) >> (i - j)));
 		}
 	    }
 	}
 
 }
 
-void unpack(uint8_t *out, const uint32_t *in) {
-	memset(out, 0x00, 32 * sizeof(uint8_t));
+void unpack(uint8_t *out, const uint64_t *in) {
+	memset(out, 0x00, 64 * sizeof(uint8_t));
 
-        for(uint8_t i = 0; i < 32; i++) {
+        for(uint8_t i = 0; i < 64; i++) {
             for(uint8_t j = 0; j < 8; j++) {
 		if((i - j) < 0) {
-		    out[i] |= ((in[j] & (0x01 << i)) << (j - i));
+		    out[i] |= (uint64_t)(((in[j] & (0x0000000000000001 << i)) << (j - i)));
 		} else {
-                    out[i] |= ((in[j] & (0x01 << i)) >> (i - j));
+                    out[i] |= (uint64_t)(((in[j] & (0x0000000000000001 << i)) >> (i - j)));
 		}
             }
         }
